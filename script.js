e6999686d53ce690689bfc9647ff39157ad9e483
agenciaHyperion <88325134+agenciaHyperion@users.noreply.github.com>
2021-08-03 09:53:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+navbar = document.querySelector('.navbar');
+hamburgerMenu = document.querySelector('.hamburger-menu');
+navbarBtn = document.querySelector('.navbar-btn');
+hamburgerMenuCloseBtn = document.querySelector('body > div > ul > li:nth-child(1) > img');
+
+navbarBtn.addEventListener('click', () => {
+    toggleHamburgerMenu();
+})
+hamburgerMenuCloseBtn.addEventListener('click', () => {
+    toggleHamburgerMenu();
+})
+
+
+function toggleHamburgerMenu() {
+
+    if (hamburgerMenu.classList.contains('open-navbar')) {
+        hamburgerMenu.classList.remove('open-navbar')
+        navbar.style.display = 'flex';
+        setTimeout(() => {
+            navbar.classList.remove('opacity-0');
+        }, 1);
+
+    } else {
+        hamburgerMenu.classList.add('open-navbar')
+        navbar.classList.add('opacity-0');
+        setTimeout(() => {
+            navbar.style.display = 'none';
+        }, 200);
+    }
+}
+
+for (let x = 0; x < hamburgerMenu.children[0].children.length; x++) {
+    let item = hamburgerMenu.children[0].children[x].children[0];
+    let nodeName = item.nodeName;
+    if (nodeName == 'A') {
+        item.addEventListener('click', () => {
+            toggleHamburgerMenu();
+        })
+    }
+}
+
+
